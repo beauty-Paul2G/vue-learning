ebe677dacdacaffe2b3aadef5dff00627a7f66a1
beauty-Paul2G <paul.garcia@solbeautyandcare.com>
2023-04-03 08:50:38 -0700

    carrio de compras añadido

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -2,8 +2,8 @@
 import ProductCard from '../components/ProductCard.vue';
 import FiltersMenu from '../components/FiltersMenu.vue'
 import LoadingIcon from '../components/icons/LoadingIcon.vue';
+
 const baseURL = "https://api.escuelajs.co/api/v1";
-const baseURLQuery = "https://api.escuelajs.co";
 
 export default {
   data() {
@@ -15,7 +15,7 @@ export default {
   },
   mounted() {
     fetch(`${baseURL}/products/`).then(res => res.json()).then(data => {
-      this.products = data.slice(0, 150);
+      this.products = data;
     });
   },
   components: {
@@ -26,26 +26,26 @@ export default {
   methods: {
     doSearch() {
       if (this.wannaSearch) {
-        this.searchResults = this.products.filter(product => product.title.toLowerCase().includes(this.searchTerm.toLowerCase()));
+        this.searchResults = this.products.filter(product =>
+          product.title.toLowerCase().includes(this.searchTerm.toLowerCase())
+        );
       }
     },
     doFilter() {
-      const minPrice = this.$refs.filtersMenu.minPrice;
-      const maxPrice = this.$refs.filtersMenu.maxPrice;
-      const data = {
-        "query": `query { products(price_min: ${minPrice}, price_max: ${maxPrice}) {id title price description images} }`
-      }
+      let minPrice = this.$refs.filtersMenu.minPrice;
+      let maxPrice = this.$refs.filtersMenu.maxPrice;
+      const category = parseInt(this.$refs.filtersMenu.selectedCategory);
 
-      fetch(`${baseURLQuery}/graphql/`, {
-        method: 'POST',
-        body: JSON.stringify(data),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      }).then(res => res.json()).then(data => {
-        this.products = data.data.products;
-        console.log(this.products)
-      })
+      minPrice = minPrice ? minPrice : 0;
+      maxPrice = maxPrice ? maxPrice : 99999;
+
+      this.products = null;
+      fetch(`${baseURL}/products/`).then(res => res.json()).then(data => {
+        this.products = data.filter(product =>
+          (product.price <= maxPrice && product.price >= minPrice) &&
+          (category === 0 ? true : product.category.id === category)
+        );
+      });
     }
   },
   computed: {
@@ -70,11 +70,15 @@ export default {
         <FiltersMenu ref="filtersMenu" />
       </aside>
       <section>
-        <span class="results-message" v-if="wannaSearch">{{ searchCount }} resultados de búsqueda para "{{ searchTerm }}":
+        <span class="results-message" v-if="wannaSearch">
+          {{ searchCount }} search results for "{{ searchTerm }}":
         </span>
         <div class="products">
-          <template v-if="products" v-for="product in (!wannaSearch ? products : searchResults)" :key="product.id">
-            <ProductCard :product="product" />
+          <template v-if="products">
+            <template v-for="product in (!wannaSearch ? products : searchResults)" :key="product.id">
+              <ProductCard :product="product" />
+            </template>
+            <h3 v-if="products && products.length === 0">There are no coincidences for those filters.</h3>
           </template>
           <LoadingIcon v-else />
         </div>
@@ -84,7 +88,7 @@ export default {
 </template>
 
 <style>
-.products-view .title {
+.products-view>.title {
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -94,6 +98,8 @@ export default {
 .products-view .title input {
   padding: 10px;
   font-size: 15px;
+  border: 1px solid #0002;
+  border-radius: 5px;
 }
 
 .products-view .title input:focus {
